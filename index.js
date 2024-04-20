a8d10b4cb5a830f59c6299913e3a3bb9ee07d7d6
robbyweeds <96435093+robbyweeds@users.noreply.github.com>
2024-04-20 13:10:13 +0000

    added folder

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
 
 app.get('/', (req, res) => {
-    res.send('Hello World!jnxdjnnc')
+    res.sendFile(path.join(__dirname, './public', 'index.html'));
   })
   
   app.listen(port, () => {
