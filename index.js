752a361c6dd2f268daa0d561c3f083099ea531ad
robby <rob@gmail.com>
2024-04-21 20:01:27 -0400

    fixed page routing

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const app = express()
 const port = 3000
 const path = require('path')
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public', 'index.html'));
+    res.sendFile(path.join(__dirname, './public/html', 'index.html'));
+  })
+
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/html', 'about.html'));
+  })
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/html', 'contact.html'));
   })
   
   app.listen(port, () => {
